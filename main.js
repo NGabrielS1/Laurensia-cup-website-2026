bb0d93d66e28fd8c656bc705fbe36dedf4cc46d5
Gabriel Sulaiman <gabrielsulaiman08@gmail.com>
2026-09-07 20:43:51 +0700

    Updated example code

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 //function for navbar
 let isNav1Visible = true;
+
+function movenav() {
+  if (isNav1Visible) {
+    document.documentElement.style.setProperty('--nav1-display', 'none');
+    document.documentElement.style.setProperty('--nav2-display', 'inline-block');
+  } else {
+    document.documentElement.style.setProperty('--nav1-display', 'inline-block');
+    document.documentElement.style.setProperty('--nav2-display', 'none');
+  }
+  isNav1Visible = !isNav1Visible;
+}
+
+//Add new js down here vvv
